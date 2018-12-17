57da112a5b75066ff405a375dad81ab5cb7fb438
yevhensydorov <assid@ukr.net>
2018-12-17 04:41:07 +0000

    add the hatch logo

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,10 @@ const Header = () => {
   return (
     <header className="App-header">
       <a href="/" className="logo">
-        Hatch
+        <img
+          src="//static1.squarespace.com/static/5937ac091b10e34a80c37c1f/t/5939c43503596e11e434e66b/1544520492504/?format=15w"
+          alt=""
+        />
       </a>
       <Navbar />
       <button className="button">Log Out</button>
